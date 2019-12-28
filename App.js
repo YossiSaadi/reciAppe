7385ff2231b40be7aa0a521c47f769687ee4084f
YossiSaadi <yossisaadi@gmail.com>
2019-12-28 17:54:29 +0200

    new app navigators order

--- App.js
+++ App.js
@@ -2,52 +2,123 @@ import React from 'react';
 import './src/firebase/fixTimerBug';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import SearchScreen from './src/screens/SearchScreen';
-import RestaurantDetailScreen from './src/screens/RestaurantDetailScreen';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import RecipeDetailedScreen from './src/screens/RecipeDetailedScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import { ThemeProvider } from 'react-native-elements';
 import reactNativeElementsTheme from './src/utils/elementsTheme';
+import NewRecipeScreen from './src/screens/NewRecipeScreen';
+import IngredientsScreen from './src/screens/IngredientsScreen';
+import { Icon } from 'react-native-elements';
 
-// const navigator = createStackNavigator(
-//   {
-//     Search: SearchScreen,
-//     Restaurant: RestaurantDetailScreen
-//   },
-//   {
-//     initialRouteName: 'Search',
-//     defaultNavigationOptions: {
-//       title: 'Business Search'
-//     }
-//   }
-// );
+import TabBarIcon from 'react-navigation-tabs/src/views/CrossFadeIcon';
 
-const AppSwitchNavigator = createSwitchNavigator({
-  LoginFlow: createStackNavigator({
-    SignIn: SignInScreen,
-    SignUp: SignUpScreen
-  }, {
-    initialRouteName: 'SignIn',
-    headerMode: 'none',
-    navigationOptions: {
-      header: null
-    }
-  }),
-
-  Main: createStackNavigator(
-    {
-      // Home: HomeScreen,
-      Search: SearchScreen,
-      Restaurant: RestaurantDetailScreen
-    },
-    {
-      initialRouteName: 'Search',
-      defaultNavigationOptions: {
-        title: 'Business Search'
+Bottom: createBottomTabNavigator({
+    Main: createStackNavigator(
+      {
+        Home: HomeScreen,
+        Recipe: RecipeDetailedScreen
+      },
+      {
+        initialRouteName: 'Home',
+        defaultNavigationOptions: {
+          title: 'Recipes Search'
+        }
       }
+    )
+  },
+  {
+    initialRouteName: 'Main',
+    defaultNavigationOptions: {
+      title: 'Something Else'
     }
-  )
-});
+  });
+
+const AppSwitchNavigator =
+  createSwitchNavigator({
+    LoginFlow: createStackNavigator({
+      SignIn: SignInScreen,
+      SignUp: SignUpScreen
+    }, {
+      initialRouteName: 'SignIn',
+      headerMode: 'none',
+      navigationOptions: {
+        header: null
+      }
+    }),
+    Bottom: createBottomTabNavigator({
+      Main: {
+        screen: createStackNavigator({
+          Home: HomeScreen,
+          Recipe: RecipeDetailedScreen
+        }, {
+          initialRouteName: 'Home',
+          navigationOptions: ({ navigation }) => {
+            return {
+              title: 'Home',
+              tabBarVisible: navigation.state.index > 0 ? false : true
+            };
+          }
+        })
+      },
+      Favorites: {
+        screen: createStackNavigator({
+          NewRecipe2: NewRecipeScreen,
+          Ingredients: IngredientsScreen
+        }, {
+          initialRouteName: 'NewRecipe2',
+          navigationOptions: ({ navigation }) => {
+            return {
+              title: 'Favorites',
+              tabBarVisible: navigation.state.index > 0 ? false : true
+            };
+          }
+        })
+      },
+      NewRecipe: {
+        screen: createStackNavigator({
+          NewRecipe2: NewRecipeScreen,
+          Ingredients: IngredientsScreen
+        }, {
+          initialRouteName: 'NewRecipe2',
+          navigationOptions: ({ navigation }) => {
+            return {
+              title: 'New',
+              tabBarVisible: navigation.state.index > 0 ? false : true
+            };
+          }
+        })
+      }
+    }, {
+      initialRouteName: 'NewRecipe',
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+          const { routeName } = navigation.state;
+          let iconName;
+          let IconComponent = Icon;
+
+          if (routeName === 'Main') {
+            iconName = 'home';
+          } else if (routeName === 'Favorites') {
+            iconName = 'star';
+          } else if (routeName === 'NewRecipe') {
+            iconName = 'plus-circle';
+          }
+
+          return <IconComponent type = 'font-awesome' name = { iconName } size = { 25 } color = { tintColor } />;
+        },
+        tabBarOptions: {
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray'
+        }
+
+      })
+    })
+  }, {
+    initialRouteName: 'Bottom'
+  });
 
 const App = createAppContainer(AppSwitchNavigator);
 
