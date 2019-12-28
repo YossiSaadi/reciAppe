a75ce012191d957a5f8f6590d3a181335e9be0f6
YossiSaadi <yossisaadi@gmail.com>
2019-12-28 17:47:23 +0200

    added the useRecipes hook to hold API search results and to make API requests to Spooncaular's API

---
+++ src/hooks/useRecipes.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import spoonacularApi, { spoonacularApiKey } from '../api/spoonacular';
+import { getRecipeByIdMock, getRecipesByTermCheeseMock, getRecipesRandomly } from '../mocks';
+
+export default () => {
+  const [ resultsFound, setResultsFound ] = useState([]);
+  const [ errorMessage, setErrorMessage ] = useState('');
+
+  const searchRecipesRandomly = async () => {
+    try {
+      const response = await spoonacularApi.recipes.get(`/random`, {
+        params: {
+          ...spoonacularApiKey,
+          number: 30
+        }
+      });
+      // const response = getRecipesRandomly;
+
+      const { recipes } = response.data;
+      setResultsFound(recipes);
+      setErrorMessage('');
+    } catch (e) {
+      setErrorMessage('Something went wrong');
+      console.log('ERROR: ' + e);
+    }
+  };
+
+  const searchRecipesByTerm = async (term) => {
+    try {
+      const response = await spoonacularApi.recipes.get(`/search`, {
+        params: {
+          ...spoonacularApiKey,
+          query: term,
+          number: 30
+        }
+      });
+      // const response = getRecipesByTermCheeseMock;
+
+      const { results } = response.data;
+      setResultsFound(results);
+      setErrorMessage('');
+    } catch (e) {
+      setErrorMessage('Something went wrong');
+      console.log('ERROR: ' + e);
+    }
+  };
+
+  const searchRecipeById = async (id) => {
+    try {
+      const response = await spoonacularApi.recipes.get(`/${ id }/information`, {
+        params: {
+          ...spoonacularApiKey
+        }
+      });
+      // const response = getRecipeByIdMock;
+
+      const results = response.data;
+      setResultsFound(results);
+      setErrorMessage('');
+    } catch (e) {
+      setErrorMessage('Something went wrong');
+      console.log('ERROR: ' + e);
+    }
+  };
+
+  return { searchRecipesByTerm, searchRecipeById, searchRecipesRandomly, resultsFound, errorMessage };
+};
