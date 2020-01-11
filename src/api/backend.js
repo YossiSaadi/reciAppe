291bd1ec3e9d4f5f84f02e604ff26ee66e17605f
Slava Kagan <slava15.92@gmail.com>
2020-01-11 22:15:57 +0200

    Add comment for changing IP while using EXPO

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//**** Change 192.168.43.162 to IP of your computer while using EXPO for Running  and Checking ****//
 export default {
   recipes: axios.create({
     baseURL: `http://192.168.43.162:9000/recipe`
